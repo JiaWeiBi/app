42d6f38e7853a51c94948b369c1760bd84457b20
seyan <961595030@qq.com>
2019-05-28 18:41:09 +0800

    .

---
+++ src/components/contentCard.vue
@@ -0,0 +1,18 @@
+<!-- 内容卡片 -->
+<template>
+    <v-container>
+
+    </v-container>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {}
+    }
+    ,props: {
+        article: Object
+        ,user: Object
+    }
+}
+</script>
