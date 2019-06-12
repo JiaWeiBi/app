355939d393fd9f058b567aaa4ed6200b7fd6bb36
seyan <961595030@qq.com>
2019-06-12 18:16:28 +0800

    .

---
+++ web-app/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+     hello
+     <router-link to="about">about</router-link>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
+
+<style>
+
+</style>
