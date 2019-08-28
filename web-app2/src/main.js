db9e93416b96e208c80ee262932e7c2f131ae353
seyan <961595030@qq.com>
2019-08-28 18:20:16 +0800

    .

--- web-app2/src/main.js
+++ web-app2/src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 
-import waterfall from 'vue-waterfall2'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './plugins/router';
 
 Vue.config.productionTip = false
 
-Vue.use(waterfall)
 
 new Vue({
   vuetify,
