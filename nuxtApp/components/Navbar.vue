5aedd1f61e8994ba4f716c1ced2968dd071d1620
seyan <961595030@qq.com>
2020-06-10 21:15:23 +0800

    。

--- nuxtApp/components/Navbar.vue
+++ nuxtApp/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div id="nav_bar">
     <nav>
       <v-app-bar app flat>
         <v-container>
           <v-row>
-            <nuxt-link to="/home" tag="span">
+            <nuxt-link to="/" class="ml-auto my-auto">
               <v-img
                 src="/logo.png"
                 class="mr-5"
@@ -15,38 +15,24 @@
                 max-width="48"
               />
             </nuxt-link>
-            <nuxt-link to="/home" tag="tab">
+            <nuxt-link to="/" class="ml-auto my-auto nav-a">
               <span class="my-auto title text-no-wrap" style="color:#2A5082;">文豪野犬</span>
             </nuxt-link>
 
-            <v-col>
-              <v-tabs>
-                <v-tab>
-                  <nuxt-link to="/" tag="tab">
-                    <span class="my-auto ml-12 hidden-sm-and-down subtitle-1">首页</span>
-                  </nuxt-link>
-                </v-tab>
-                <v-tab>
-                  <nuxt-link to="/">
-                    <span class="my-auto ml-12 hidden-sm-and-down subtitle-1">XXX</span>
-                  </nuxt-link>
-                </v-tab>
-                <v-tab>
-                  <nuxt-link to="/">
-                    <span class="my-auto ml-12 hidden-sm-and-down subtitle-1">XXX</span>
+            <v-col class="ml-12">
+              <v-tabs color="black">
+                <v-tab v-for="(item, index) in navList" :key="item.url+index">
+                  <nuxt-link :to="item.url">
+                    <span class="my-auto ml-auto hidden-sm-and-down subtitle-1">{{ item.name }}</span>
                   </nuxt-link>
                 </v-tab>
               </v-tabs>
             </v-col>
-            <v-btn icon>
+            <v-btn icon class="ml-auto my-auto">
               <v-icon>mdi-magnify</v-icon>
             </v-btn>
-            <v-btn>
-              <span class="my-auto ml-12 hidden-sm-and-down subtitle-1">登陆</span>
-            </v-btn>
-            <v-btn>
-              <span class="my-auto ml-12 hidden-sm-and-down subtitle-1">注册</span>
-            </v-btn>
+            <v-btn class="ml-auto my-auto" text color="deep-purple accent-4">登陆</v-btn>
+            <v-btn class="ml-auto my-auto" text color="deep-purple accent-4">注册</v-btn>
           </v-row>
         </v-container>
       </v-app-bar>
@@ -63,6 +49,11 @@ export default {
   },
   data() {
     return {
+      navList: [
+        { name: "首页", url: "/index" },
+        { name: "XXX", url: "/xxx" },
+        { name: "XXX", url: "/xxx" }
+      ],
       justify: ["start", "center", "space-around"]
     };
   },
@@ -75,10 +66,15 @@ export default {
 </script>
 
 <style scoped>
+#nav_bar a {
+  text-decoration: none;
+}
+
 .ch {
   font-family: "font5";
   font-size: 2vw;
 }
+
 @media (min-width: 992px) and (max-width: 1300px) {
   .ch {
     font-size: 2vw;
