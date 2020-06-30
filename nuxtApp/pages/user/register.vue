a2eebd53cc70512f6fa4a187cc97d1409810c599
seyan <961595030@qq.com>
2020-06-30 18:35:58 +0800

    。

--- nuxtApp/pages/user/register.vue
+++ nuxtApp/pages/user/register.vue
@@ -12,13 +12,43 @@
             >首先欢迎您来到本站，由于本站目前处于试运营阶段，有些地方还有待完善，使用过程中遇到任何问题或者有任何意见和建议都欢迎您通过站内反馈，或者发送邮件到961595030@qq.com，也可以直接联系QQ 961595030 并注明来意</v-alert>
           </v-col>
           <v-col cols="12">
-            <v-text-field label="昵称*" :rules="[rules.required, rules.nickname]" outlined counter maxlength="20"></v-text-field>
+            <v-text-field
+              label="昵称*"
+              :rules="[rules.required, rules.nickname]"
+              outlined
+              counter
+              maxlength="20"
+            ></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-text-field label="邮箱*" :rules="[rules.required, rules.email]" validate-on-blur outlined></v-text-field>
+            <v-text-field
+              label="邮箱*"
+              :rules="[rules.required, rules.email]"
+              validate-on-blur
+              outlined
+            ></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-text-field label="密码*" type="password" outlined></v-text-field>
+            <v-text-field
+              label="密码*"
+              :rules="[rules.required, rules.password]"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="show = !show"
+              :type="show ? 'text' : 'password'"
+              validate-on-blur
+              outlined
+            ></v-text-field>
+            <span>*密码由6-12位字母、数字组合*</span>
+          </v-col>
+          <v-col cols="12">
+            <v-row>
+              <v-btn color="warning" @click="register">注册</v-btn>
+              <v-spacer></v-spacer>
+              <span>
+                注册即代表同意
+                <nuxt-link to="/user/term">用户使用条款</nuxt-link>
+              </span>
+            </v-row>
           </v-col>
         </v-row>
       </v-col>
@@ -30,18 +60,34 @@
 export default {
   data() {
     return {
-       rules: {
-          required: value => !!value || '必填项.',
-          counter: value => value.length <= 20 || '最多20个字符',
-          email: value => {
-            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            return pattern.test(value) || '非法邮箱地址'
-          },
-          nickname: async value => {
-            return true;
+      show: false,
+      rules: {
+        required: value => !!value || "必填项.",
+        counter: value => value.length <= 20 || "最多20个字符",
+        password: value => {
+          if (!value) {
+            return "必填项";
           }
+          let reg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/;
+          if (!reg.test(value)) {
+            return "密码不符合要求";
+          }
+          return true;
         },
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "非法邮箱地址";
+        },
+        nickname: async value => {
+          return true;
+        }
+      }
     };
+  },
+  methods: {
+    register: async function() {
+      alert("===")
+    }
   }
 };
 </script>
---
+++ nuxtApp/pages/user/term.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-container class="fill-height" fluid>
+        <span class="mx-auto">不是吧，阿sir，使用条款你都要看？<br/>
+        哈哈，没想到吧，条款我还没想好
+        </span>
+    </v-container>
+</template>
